587cf612d0b98ef445fec43d5b35bd438534a292
Debora Cristina Souza Santos <debora.santos@capgemini.com>
2024-01-29 14:36:15 -0300

    Correcao

--- src/application/Program.java
+++ src/application/Program.java
@@ -12,7 +12,7 @@ public class Program {
 
 		Department obj = new Department(1, "Books");
 		
-		Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.00, obj);
+		Seller seller = new Seller(21, "Bob", "bob@gmail.com",  new Date(), 3000.00, obj);
 
 		System.out.println(seller);
 		
--- src/model/entities/Seller.java
+++ src/model/entities/Seller.java
@@ -1,12 +1,11 @@
 package model.entities;
 
 import java.io.Serializable;
-import java.sql.Date;
+import java.util.Date;
 import java.util.Objects;
 
 public class Seller implements Serializable{
 	
-	private static final long serialVersionUID = 1L;
 	private Integer id;
 	private String name;
 	private String email;
@@ -75,7 +74,22 @@ public class Seller implements Serializable{
 		this.department = department;
 	}
 
-	
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Seller other = (Seller) obj;
+		return Objects.equals(id, other.id);
+	}
 
 	@Override
 	public String toString() {
